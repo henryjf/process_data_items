e9163a69a338d9c2bf47b32fcb03bef304d90f60
JEFF HENRY <henry_jf@hotmail.com>
2016-03-04 11:07:21 -0500

    worked on problem 1

--- js/main.js
+++ js/main.js
@@ -19,32 +19,46 @@
 //  gpa: 2.7
 //}
 
-var students = [studentA, studentB, studentC, studentD];
-
-function averageGPA(pupils) {
-  var gpaSum = pupils.reduce(function(totalGPA, currentStudent){
-    return totalGPA + currentStudent.gpa;
-  }, 0);
-  var average = gpaSum / pupils.length;
-  return average;
-}
-
-var avgGpaSpan = document.querySelector('.average-gpa');
-var passGpaSpan = document.querySelector('.average-passing-gpa');
-
-avgGpaSpan.textContent = averageGPA(students);
-//
-
-function averagePrice(items);
-  var itemSum= price.reduce(function(totalPrice, currentItem){
-    return totalPrice + currentItem.price;
-  }, 0)
-var average = itemsSum / numOfItems.length;
-return average;
-}
+//items.forEach(function(obj) {
+  //console.log(obj.price) });
 
 
+// }
+// var students = [studentA, studentB, studentC, studentD];
+
+// function averageGPA(pupils) {
+//   var gpaSum = pupils.reduce(function(totalGPA, currentStudent){
+//     return totalGPA + currentStudent.gpa;
+//   }, 0);
+//   var average = gpaSum / pupils.length;
+//   return average;
+// }
+
+// var avgGpaSpan = document.querySelector('.average-gpa');
+// var passGpaSpan = document.querySelector('.average-passing-gpa');
+
+// avgGpaSpan.textContent = averageGPA(students);
+// //
+
+// function averagePrice(items);
+//   var itemSum= price.reduce(function(totalPrice, currentItem){
+//     return totalPrice + currentItem.price;
+//   }, 0)
+// var average = itemsSum / numOfItems.length;
+// return average;
+// }
 
 
-
+// Problem 1
+var prices = items.map(function(obj){
+  return obj.price;
+});
+var sumPrices = prices.reduce(function(prev, next){
+  return prev + next;
+});
+var avgPrice = sumPrices / items.length;
+// console.log(avgPrice);
+// Want to round avgPrica
+var str ="The average price is $" + avgPrice + ".";
+console.log(str);
 

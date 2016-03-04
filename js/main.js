eb31e397cc5e3fec44c145270f3f0bb2e294cc79
JEFF HENRY <henry_jf@hotmail.com>
2016-03-04 14:09:57 -0500

    answered q's 1 and 2

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-//var studentA = {
- // grade: 100,
- // name: 'Leonard',
- // gpa: 4.0
-//}
-//var studentB = {
-  //grade: 10,
-  //name: 'George',
-  //gpa: 1.0
-//}
-//var studentC = {
-  //grade: 88,
-  //name: 'Karla',
-  //gpa: 3.5
-//}
-//var studentD = {
-//  grade: 72,
-//  name: 'Fred',
-//  gpa: 2.7
-//}
+
 
 //items.forEach(function(obj) {
   //console.log(obj.price) });
@@ -57,8 +38,30 @@ var sumPrices = prices.reduce(function(prev, next){
   return prev + next;
 });
 var avgPrice = sumPrices / items.length;
-// console.log(avgPrice);
-// Want to round avgPrica
+//numAvgPrice.toFixed(avgPrice);
+
+ //console.log(avgPrice);
+// Want to round avgPrice
 var str ="The average price is $" + avgPrice + ".";
 console.log(str);
+
+// Problem 2
+
+//get all prices
+var pricesSet = items.map(function(obj){
+  return obj.price;
+})
+//filter prices 
+var setPrice = pricesSet.filter(function(price){
+  return 14 < price && price < 18;
+})
+
+console.log(pricesSet);
+console.log(setPrice);
+
+
+
+
+
+
 

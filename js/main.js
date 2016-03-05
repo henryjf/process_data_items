261b272cbff584c168be2ac7a7da042437f92722
JEFF HENRY <henry_jf@hotmail.com>
2016-03-05 12:54:31 -0500

    answered problem 3

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ var avgPrice = sumPrices / items.length;
 //numAvgPrice.toFixed(avgPrice);
 
  //console.log(avgPrice);
+
 // Want to round avgPrice
 var str ="The average price is $" + avgPrice + ".";
 console.log(str);
@@ -29,7 +30,6 @@ return item.price > 14 && item.price < 18;
 })
 
 var titles = midPriceProducts.map(function(obj){
-
   return obj.title
 })
 
@@ -40,13 +40,28 @@ console.log(titles);
 // Problem 3
 //Which item has a "GBP" currency code? Display it's name and price.
 
-//filter items to get GBP currency 
-var britishItem = items.filter(function(currency){
-  return currency.currencyCode;
+//find all currency code items;
+//filter currency items to get GBP item
+//link GBP item to its title 
+//print out string w title and price
+
+var britishItem = items.filter(function(obj){
+return obj.currency_code === "GBP";
 })
 
+var currency = britishItem.map(function(item){
+   return item.title;
+ })
 
-console.log(GBP);
+var str = currency + " costs £18";
+
+console.log(str);
+
+
+//Problem 4
+//Display a list of all items who are made of wood.
+
+
 
 
 
